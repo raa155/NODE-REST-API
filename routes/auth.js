8816e252a376e6c2c89d667d56bf91af46b4d0af
Roman <roman.a.abad@gmail.com>
2023-04-23 01:01:52 -0400

    create user schema model | created a registration system that checks if user is found in DB and if password is valid | added password hashing using bcrypt for added security.

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,45 @@
+// @ts-nocheck
 const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-router.get('/', (req, res) => {
-   res.send('hey its auth routes');
-})
+// REGISTER
+router.post('/register', async (req, res) => {
+   try {
+      //generate new password
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(req.body.password, salt);
+      //create new user
+      const newUser = new User({
+         username: req.body.username,
+         email: req.body.email,
+         password: hashedPassword,
+      });
+      //save user and respond
+      const user = await newUser.save();
+      res.status(200).json(user);
+   } catch (err) {
+      res.status(500).json(err);
+   }
+});
+
+
+//LOGIN
+router.post('/login', async (req, res) => {
+   try {
+      // check if user exists
+      const user = await User.findOne({ email: req.body.email });
+      !user && res.status(404).send("user not found")
+      //check if user password is valid
+      const validPassword = await bcrypt.compare(req.body.password, user.password)
+      !validPassword && res.status(400).json("wrong password")
+
+      //if user enter valid email and password
+      res.status(200).json(user);
+   } catch (err) {
+      res.status(500).json(err);
+   }
+});
+
 
 module.exports = router;
